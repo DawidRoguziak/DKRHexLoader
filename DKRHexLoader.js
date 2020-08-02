e3532b74d01cc124833a6e9b79a1768ba73468b3
Dawid Roguziak <dawid.roguziak@live.umcs.edu.pl>
2020-08-02 19:12:30 +0200

    npm publish

--- DKRHexLoader.js
+++ DKRHexLoader.js
@@ -62,3 +62,5 @@ class DKRHexLoader {
     }
     
 }
+
+export default DKRHexLoader;
---
+++ index.js
@@ -0,0 +1,5 @@
+function DKRLoader(options) {
+     return new DKRHexLoader(options);
+}
+
+module.export.DKRLoader = DKRLoader(options);
